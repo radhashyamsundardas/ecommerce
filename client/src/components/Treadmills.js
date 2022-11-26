a09538fbc3599350143224a1dc1ebe1f76c2b2ea
hiren patel <hirenpatel0526@yahoo.com>
2022-11-25 20:24:58 -0800

    added 2 products in treadmill section

--- client/src/components/Treadmills.js
+++ client/src/components/Treadmills.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 function Treadmills() {
   return (
-    <div>Treadmills</div>
+    <div>
+
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcREGdoAxg_zjQu5d7kXg0LyO2hhQUI-WM8iBcN0UuTcWxtmsfPtHUDBS9-xS6mSE9bccWLMhwIbReB3W90KAwMiWUkSOuiy4eRvgApyst00LdqtDrueYFKq&usqp=CAE" height={200}/>
+    <Card.Body>
+      <Card.Title>Weslo Cadence G 5.9i Folding Treadmill</Card.Title>
+      <Card.Text>
+      Space saver design: fold it up and walk away in 1 easy step
+Integrated tablet holder: keeps your tablet secure and conveniently close; Treadmill accommodates up to 275 pounds
+16 inches x 50 inches tread belt: perfect for users of all heights, you'll be comfortable while you reach your goals
+The console must be active by registering
+      </Card.Text>
+      <Button variant="primary">ADD TO CART</Button>
+
+      <Card.Img variant="top" src="https://i5.walmartimages.com/asr/35e3af0c-1239-449b-b177-4e919af53d11.8482347832d0f776331a1ba7f4b47fed.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF" height={200}/>
+      <Card.Title>SEGMART Electric Treadmill </Card.Title>
+      <Card.Text>
+      Wide Tread Belt, EKG Grip Pulse Sensor, Easy Assembly Home Motorized Running Machine with 12 Preset Program, 240 lbs, S1681
+      </Card.Text>
+      <Button variant="primary">ADD TO CART</Button>
+
+      
+    </Card.Body>
+  </Card>
+
+    </div>
+
+
+
+
+
+
+
+
+  
   )
 }
 
