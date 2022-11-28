d9a812e496e81ddaccbdc253ba848a0da6747691
Allison <farrella95@gmail.com>
2022-11-28 17:50:01 -0500

    still in progress

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,73 +1,55 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className='footer-bg-light fixed-bottom'>
-     <div >
-            
-            <ul>
-                <li  class="-item">
-                    <Link to="/SalesAssitance">
-                        <div >
-                            <h5>SALES ASSISTANCE</h5>
-                            <ul>
-                              <li>Call us at 888-888-8888</li>
-                              
-                              <li>Office Hours:<br/> Monday-Friday: 9AM-5PM EST</li>
-                            </ul>
-                        </div>
-                    </Link>
-                </li>
+    <footer>
+      <div class="-item">
+        <Link to="/SalesAssitance">SALES ASSISTANCE</Link>
+        <ul>
+          <li>Call us at 888-888-8888</li>
 
-                <li class="-item">
-                    <Link to="/CustomerService">
-                        <div >
-                            <h5>CUSTOMER SERVICE</h5>
-                            <ul>
-                              <li>Power Rack Comparison</li>
-                              <li>Contact Us</li>
-                              <li>Warranty</li>
-                              <li>Return Policy</li>
-                              <li>Shipping Policy</li>
+          <li>
+            Office Hours:
+            <br /> Monday-Friday: 9AM-5PM EST
+          </li>
+        </ul>
+      </div>
 
-                            </ul>
-                        </div>
-                    </Link>
-                </li>
+      <div class="-item">
+        <Link to="/CustomerService">CUSTOMER SERVICE</Link>
+        <ul>
+          <li>Power Rack Comparison</li>
+          <li>Contact Us</li>
+          <li>Warranty</li>
+          <li>Return Policy</li>
+          <li>Shipping Policy</li>
+        </ul>
+      </div>
 
-                <li class="-item">
-                    <Link to="/Account">
-                        <div >
-                            <h5>ACCOUNT</h5>
-                            <ul>
-                              <li>My Account</li>
-                              <li>Track Order</li>
-                              <li>Reward Points</li>
-                              <li>Privacy</li>
-                            </ul>
-                        </div>
-                    </Link>
-                </li>
+      <div class="-item">
+        <Link to="/Account">ACCOUNT</Link>
+        <ul>
+          <li>My Account</li>
+          <li>Track Order</li>
+          <li>Reward Points</li>
+          <li>Privacy</li>
+        </ul>
+      </div>
 
-                <li class="-item">
-                    <Link to="/About">
-                        <div >
-                            <h5>ABOUT</h5>
-                            <ul>
-                              <li>About Us</li>
-                              <li>Careers</li>
-                              <li>New Product Request</li>
-                              <li>Financing</li>
-                            </ul>
-                        </div>
-                    </Link>
-                </li>
-            </ul>
-            <h3>2022 Geogia Tech Students. All Rights Reserved</h3>
-    </div>
-</footer>
-  )
+      <div class="-item">
+        <Link to="/About">ABOUT</Link>
+        <ul>
+          <li>About Us</li>
+          <li>Careers</li>
+          <li>New Product Request</li>
+          <li>Financing</li>
+        </ul>
+      </div>
+
+      <span>Geogia Tech Students © 2022</span>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
