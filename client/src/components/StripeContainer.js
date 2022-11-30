0350afc497abba3cd6b8e8247d7ecca74534ef06
Nordleen De Frias <nordleendefrias@gmail.com>
2022-11-29 23:55:37 -0500

    added Stripe implementation still in testing phase

---
+++ client/src/components/StripeContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+import PaymentForm from './PaymentForm';
+
+const PUBLIC_KEY = "pk_test_51M9a9ECHX2BJ04HblijMBqUxRF0HuowMdlaLLMC2CO0fcKYILSIxdm5i9LrkIjlPThkB3rALjzNB9sR6lylgMmv400TdyXuG4T";
+const stripeTestPromise = loadStripe(PUBLIC_KEY);
+
+export default function StripeContainer() {
+    return (
+        <Elements stripe={stripeTestPromise}>
+            <PaymentForm />    
+        </Elements>
+
+    )
+}
