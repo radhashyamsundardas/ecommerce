0c2d115225a88dbe0ccf4d59159f1b2ff4ef85be
hiren patel <hirenpatel0526@yahoo.com>
2022-11-29 19:53:27 -0800

    added action,globalstate,helper and reducer js in utils folder

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,34 +16,41 @@ const styles = {
 export default function bar() {
   return (
     <nav className="navbar navbar-expand-lg py-0 navbar-dark bg-dark static-top">
-       <div className="container">
-      <Link to="/Home">
-      <img className="d-inline-block align-middle mr-2" style={styles.pic} src={logo} alt="Company logo" />
-      </Link>
-      <div id="navbarSupportedContent" className="collapse navbar-collapse">
-      <ul className="navbar-nav ml-auto text-center">
-        
-        <li className="nav-item">
-          <Link className="link-to" to="/Treadmills">TREADMILLS</Link>
-        </li>
+      <div className="container">
+        <Link to="/Home">
+          <img className="d-inline-block align-middle mr-2" style={styles.pic} src={logo} alt="Company logo" />
+        </Link>
+        <div id="navbarSupportedContent" className="collapse navbar-collapse">
+          <ul className="navbar-nav ml-auto text-center">
 
-        <li className="nav-item">
-          <Link className="link-to" to="/Bikes">BIKES</Link>
-        </li>
+            <li className="nav-item">
+              <Link className="link-to" to="/Treadmills">TREADMILLS</Link>
+            </li>
 
-        <li className="nav-item">
-          <Link className="link-to" to="/Shoes">SHOES</Link>
-        </li>
+            <li className="nav-item">
+              <Link className="link-to" to="/Bikes">BIKES</Link>
+            </li>
 
-        <li className="nav-item">
-          <Link className="link-to" to="/Contactus">CONTACT US</Link>
-        </li>
+            <li className="nav-item">
+              <Link className="link-to" to="/Shoes">SHOES</Link>
+            </li>
 
-        <li className="nav-item active">
-          <Link className="link-to" to="/LOGIN">LOGIN/SIGN UP</Link>
-        </li>
-      </ul>
-      </div>
+            <li className="nav-item">
+              <Link className="link-to" to="/Contactus">CONTACT US</Link>
+            </li>
+
+            <li className="nav-item active">
+              <Link className="link-to" to="/LOGIN">LOGIN/SIGN UP</Link>
+            </li>
+
+
+            <li className="nav-item active">
+              <Link className="link-to" to="/Cart">Shopping Cart</Link>
+            </li>
+          
+           
+          </ul>
+        </div>
       </div>
     </nav>
   );
---
+++ client/src/components/cart.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import Treadmills from './Treadmills'
+
+
+export default function cart(props) {
+  const {cartItems} = props;
+  return (
+ <aside>
+  <h2>Cart Items</h2>
+  
+  <div>
+  </div>
+ </aside>
+  )
+}
+
