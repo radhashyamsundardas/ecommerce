7dd64ce5c794a29b465aa99c1347438e3b169a73
tamekamclean <tamekasmclean@gmail.com>
2022-11-30 10:25:26 -0500

    some errors fixed

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,7 +23,7 @@ const typeDefs = gql `
         category: String
         productId: ID
         quantity: Int
-        username: user
+        username: User
     }
 
     type Product {
@@ -54,21 +54,20 @@ const typeDefs = gql `
     }
 
     type Query {
-        user: [User]
+        users: [User]
         user(username: String!): User
-        order(username: String!): [Order]
+        orders(username: String!): [Order]
         order(orderId: ID!): Order
-        cart(username: String!): [Cart]
+        carts(username: String!): [Cart]
         cart(cartId: ID!): Cart
-        product: [Product]
-        product(productId: ID!): Product
+        products: [Product]
         product(username: String!): [Product]
     }
 
     type Mutation {
-        addUser(username: String!, address: String!, city: String!, state: String!, zipcode: String!, country: String!, telephone: String!, email: String!, password: String!): Auth
+        addUser(username: String!, address: String!, city: String!, state: String!, zipcode: String!, country: String!, telephone: String!, email: String!, password: String!): User
         updateUser(username: String!, address: String!, city: String!, state: String!, zipcode: String!, country: String!, telephone: String!, email: String!, password: String!): User
-        login(email: String!, password: String!): Auth
+        login(email: String!, password: String!): User
         addProduct(productName: String!, productDescription: String!, stockQty: Int!, price: Float!, category: String!, image_link: String!): Product
         updateProduct(id: ID!, productName: String!, productDescription: String!, stockQty: Int!, price: Float!, category: String!, imageLink: String!) : Product
         removeProduct(id: ID!): Product
@@ -79,6 +78,8 @@ const typeDefs = gql `
         removecart(id: ID!): Cart       
     }
 `;
+
+// product(productId: ID!): Product {was in line 64}
 
 module.exports = typeDefs;
 
